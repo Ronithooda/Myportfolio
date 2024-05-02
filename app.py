181b8ba7381dfe5d1860fffd03f59383a28fc90a
Ronithooda <150799062+Ronithooda@users.noreply.github.com>
2024-05-02 16:12:01 +0530

    added intial files

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request,redirect
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
